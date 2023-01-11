0193a793be1caeef25546d96439d47ae5aec7222
nainabbas <nainabbas5@gmail.com>
2023-01-11 18:10:56 +0500

    revert fullscreen fix

--- public/js/RoomClient.js
+++ public/js/RoomClient.js
@@ -148,7 +148,7 @@ class RoomClient {
 
         this._isConnected = false;
         this.isVideoOnFullScreen = false;
-        this.isVideoFullScreenSupported = true;
+        this.isVideoFullScreenSupported = peer_info.is_mobile_device && peer_info.os_name === 'iOS' ? false : true;
         this.isChatOpen = false;
         this.isChatEmojiOpen = false;
         this.showChatOnMessage = true;
